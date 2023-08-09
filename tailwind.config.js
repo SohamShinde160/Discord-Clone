61c00210a9028c14b901e49034d61aa65de0f943
SohamShinde160 <shindesoham390@gmail.com>
2023-08-09 10:39:05 +0530

    discord clone assets

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontfamily: {
+    Whitney: ["Whitney"],
+    Ginto: ["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
